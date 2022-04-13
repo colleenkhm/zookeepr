0e73db69ce5a3ae1b5a0540b1cc2962955dbee2c
Colleen Murray <colleenkhm@gmail.com>
2022-04-13 18:36:28 -0500

    create heroku app

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
@@ -29,7 +30,7 @@ function filterByQuery(query, animalsArray) {
             );
         });
     }
-    
+
     if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
@@ -50,6 +51,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!')
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
 });
